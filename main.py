3c01238f17cb3c2185f246c93828a2120cc00aa7
HanouneCamara <Alicmr13@gmail.com>
2023-11-19 12:26:26 +0000

    end

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from PIL import Image, ImageEnhance, ImageFilter
 import os
 
+path = './imgs'
+
+
+for filename in os.listdir(path):
+    img = Image.open(f"{path}/{filename}")
+    
+    edit = img.filter(ImageFilter.SHARPEN).convert('L').rotate(90)
+    clean_name = os.path.splitext(filename)[0]
+    edit.save(f'./editedImgs/{clean_name}_edited.jpg')
+    
